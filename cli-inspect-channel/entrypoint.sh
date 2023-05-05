f478b601d682c0e04cf6328cc6ffdd39ec28723c
Josh De Winne <josh@dewinne.dev>
2023-05-05 12:58:38 -0700

    Adding channel slug to inspect channel

--- cli-inspect-channel/entrypoint.sh
+++ cli-inspect-channel/entrypoint.sh
@@ -3,6 +3,8 @@
 /replicated $@ > channel.json
 
 CHANNEL_ID=$(cat channel.json | jq -r '.id')
+CHANNEL_SLUG=$(cat channel.json | jq -r '.slug')
 RELEASE_SEQUENCE=$(cat channel.json | jq -r '.releaseSequence')
 echo "channel-id=$CHANNEL_ID" >> $GITHUB_OUTPUT
+echo "channel-slug=$CHANNEL_SLUG" >> $GITHUB_OUTPUT
 echo "release-sequence=$RELEASE_SEQUENCE" >> $GITHUB_OUTPUT
